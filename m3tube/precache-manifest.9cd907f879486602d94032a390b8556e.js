c7794cd2392f9a2213d22feba1f849a7cde45627
MD AL AMIN <m3alamin@gmail.com>
2020-01-21 18:00:09 +0600

    m3tube v3.0.0

--- m3tube/precache-manifest.9cd907f879486602d94032a390b8556e.js
+++ m3tube/precache-manifest.9cd907f879486602d94032a390b8556e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "24e5fba8c8457de02408d184368ea3ea",
+    "revision": "84c56394681a90fa4d361e1a17fbebd2",
     "url": "/index.html"
   },
   {
-    "revision": "97c3f1994c357a270100",
+    "revision": "7cae3af92ffed04d546e",
     "url": "/static/css/main.16bb9f63.chunk.css"
   },
   {
-    "revision": "f641322b0bc9b9ec8018",
-    "url": "/static/js/2.f4d6f24b.chunk.js"
+    "revision": "66c5bbcc42fe0c0b6e18",
+    "url": "/static/js/2.7cdd5b5c.chunk.js"
   },
   {
     "revision": "e928fe768baa9832b5bc57eae021f30c",
-    "url": "/static/js/2.f4d6f24b.chunk.js.LICENSE"
+    "url": "/static/js/2.7cdd5b5c.chunk.js.LICENSE"
   },
   {
-    "revision": "97c3f1994c357a270100",
-    "url": "/static/js/main.1f263240.chunk.js"
+    "revision": "7cae3af92ffed04d546e",
+    "url": "/static/js/main.142933b3.chunk.js"
   },
   {
     "revision": "947178f7945a23e0de48",
--- m3tube/service-worker.js
+++ m3tube/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d90f20d2ece51fdf7a0e8d6c09f76540.js"
+  "/precache-manifest.9cd907f879486602d94032a390b8556e.js"
 );
 
 self.addEventListener('message', (event) => {
